aae1d3b8e44876821ca8b7b00b413d436a7217ee
william velasquez <williamvelasquez@outlook.com>
2020-09-24 20:41:17 -0400

    Extras: Agregado interoperabilidad con JavaScript y refactorezacion de codigo Eliminadas clases y namespaces

---
+++ BlazingPizza.ComponentsLibrary/IJSRuntimeExtension.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.ComponentsLibrary
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async Task EvalVoidAsync(this IJSRuntime jsRuntime, string script)
+        {
+            if (script.Length > 0)
+            {
+                await jsRuntime.InvokeVoidAsync("eval", script);
+            }
+        }
+
+        public static async Task<T> EvalAsync<T>(this IJSRuntime jsRuntime, string script)
+        {
+            T result = default;
+            if (script.Length > 0)
+            {
+                script = $"(function() {{{script}}})()";
+                result = await jsRuntime.InvokeAsync<T>("eval", script);
+            }
+            return result;
+        }
+
+        public static ValueTask<bool> Confirm(this IJSRuntime jsRuntime, string message)
+        {
+            return jsRuntime.InvokeAsync<bool>("confirm", message);
+        }
+
+        public static ValueTask<bool> Focus2(this IJSRuntime jsRuntime, string Id)
+        {
+            //                                        focus2
+            return jsRuntime.InvokeAsync<bool>("focus2", Id);
+        }
+
+  
+        public static ValueTask<T> GetAsync<T>(IJSRuntime jSRuntime, string key) =>
+            jSRuntime.InvokeAsync<T>("blazorJSFunctions.getLocalStorage", key);
+
+        public static ValueTask SetAsync(
+            IJSRuntime jSRuntime, string key, object value) =>
+            jSRuntime.InvokeVoidAsync("blazorJSFunctions.setLocalStorage", key, value);
+
+        public static ValueTask DeleteAsync(IJSRuntime jSRuntime, string key) =>
+            jSRuntime.InvokeVoidAsync("blazorJSFunctions.deleteLocalStorage", key);
+    }
+}
---
+++ BlazingPizza.ComponentsLibrary/wwwroot/functions.js
@@ -0,0 +1,18 @@
+﻿(
+    function () {
+        window.blazorJSFunctions =
+        {
+            focus: (selector) =>
+            {
+                var element = document.querySelector(selector)
+                if (element)
+                {
+                    element.focus();
+                };
+            },
+            getLocalStorage: key => key in localStorage ? JSON.parse(localStorage[key]) : null,
+            setLocalStorage: (key, value) => localStorage[key] = JSON.stringify(value),
+            deleteLocalStorage: key => delete localStorage[key]
+        };
+    }
+)();
